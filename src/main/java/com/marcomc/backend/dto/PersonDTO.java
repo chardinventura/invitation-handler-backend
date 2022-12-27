20ac1259cd6e1e073275861bff52ea69d671134a
chardindelacruz <chardin.delacruz-ventura@viseo.com>
2022-12-27 11:08:04 -0400

    fix: carecteres especiales admitidos para el nombre y el apellido

--- src/main/java/com/marcomc/backend/dto/PersonDTO.java
+++ src/main/java/com/marcomc/backend/dto/PersonDTO.java
@@ -13,9 +13,9 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PersonDTO {
 	private long id;
-	@Pattern(regexp = "[a-zA-Z0-9\s]{1,}", message = "{validation.firstName}")
+	@NotBlank(message = "{validation.firstName}")
 	private String firstName;
-	@Pattern(regexp = "[a-zA-Z0-9\s]{1,}", message = "{validation.lastName}")
+	@NotBlank(message = "{validation.lastName}")
 	private String lastName;
 	private boolean attendance;
 	@NotBlank(message = "{validation.notNull}")
